006c62755a96bbed63b98e91e3925595406f5538
Henrique Duran <henriqueduran15@gmail.com>
2025-12-21 23:44:29 -0300

    triangles

--- triangles.hs
+++ triangles.hs
@@ -1,3 +1,29 @@
-triangles = [(a, b, c) | a <- [1 .. 10], b <- [1 .. 10], c <- [1 .. 10], a + b < c || a + c < b || b + c < a]
+import Data.List
 
-rightTriangles = [(a, b, c) | a <- [1 .. 10], b <- [1 .. 10], c <- [1 .. 10], a^2+b^2==c^2]
+triples :: [(Int, Int, Int)]
+triples =
+  [ (a, b, c)
+    | a <- [1 .. 10],
+      b <- [1 .. 10],
+      c <- [1 .. 10],
+      triangle a b c
+  ]
+
+triangle :: Int -> Int -> Int -> Bool
+triangle a b c =
+  let m = maximum [a, b, c]
+   in a + b + c > 2 * m
+
+rightTriangle :: Int -> Int -> Int -> Bool
+rightTriangle a b c =
+  triangle a b c
+    && let [x, y, z] = sort [a, b, c]
+        in (x ^ 2 + y ^ 2 == z ^ 2)
+
+listTriangles :: [(Int, Int, Int)]
+listTriangles =
+  filter (\(a,b,c) -> triangle a b c) triples
+
+listRightTriangles :: [(Int, Int, Int)]
+listRightTriangles =
+  filter (\(a,b,c) -> rightTriangle a b c) triples
