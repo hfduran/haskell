93cd2afd621855690c98fc60052c90d66037d0b0
Henrique Duran <henriqueduran15@gmail.com>
2025-12-30 15:05:56 -0300

    maps

---
+++ maps.hs
@@ -0,0 +1,11 @@
+demo1 :: [Integer]
+demo1 = map (+ 3) [1, 2, 3, 4]
+
+mySum x y = x + y
+
+demo2 :: [Integer]
+demo2 = map (mySum 1) [1, 2, 3, 4]
+
+-- the very same as...
+demo3 :: [Integer]
+demo3 = [mySum 1 x | x <- [1,2,3,4]]
