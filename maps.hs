5778defa496ec0d09b272384e41aeffc75764bcd
Henrique Duran <henriqueduran15@gmail.com>
2025-12-31 15:38:48 -0300

    maps again

--- maps.hs
+++ maps.hs
@@ -8,4 +8,7 @@ demo2 = map (mySum 1) [1, 2, 3, 4]
 
 -- the very same as...
 demo3 :: [Integer]
-demo3 = [mySum 1 x | x <- [1,2,3,4]]
+demo3 = [mySum 1 x | x <- [1, 2, 3, 4]]
+
+demo4 :: [Integer]
+demo4 = map (\x -> x + 1) [1, 2, 3, 4]
